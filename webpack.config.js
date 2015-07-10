304e824c530e6e5cd41333de407f85298236e123
zhangqinghong <lubezhang@gmail.com>
2015-07-10 18:49:47 +0800

    调整打包逻辑

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,12 @@ var srcPath = __dirname + '/src/';
 var distPath = __dirname + '/dist/';
 
 module.exports = {
-    // entry: srcPath + 'indexBabel.js',
-    entry: [
-      'webpack-dev-server/client?http://127.0.0.1:3000', // WebpackDevServer host and port
-      'webpack/hot/only-dev-server',
-      srcPath + 'indexBabel.js' // Your appʼs entry point
-    ],
+    entry: srcPath + 'indexBabel.js',
+    // entry: [
+    //   'webpack-dev-server/client?http://127.0.0.1:3000', // WebpackDevServer host and port
+    //   'webpack/hot/only-dev-server',
+    //   srcPath + 'indexBabel.js' // Your appʼs entry point
+    // ],
 
     output: {
         path: distPath,
